7a038136a3168108653dbf72359d710908dfa994
MichaelChristenson <mochristenson@gamil.com>
2017-12-29 12:36:20 -0600

    Initial commit

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+setup(
+    name='seaborn_file',
+    version='0.0.1',
+    description='SeabornFile enables the manipulation of the'
+                'directories of a computer within a program.',
+    long_description='',
+    author='Ben Christenson',
+    author_email='Python@BenChristenson.com',
+    url='https://github.com/SeabornGames/File',
+    download_url='https://github.com/SeabornGames/File'
+                 '/tarball/download',
+    keywords=['os'],
+    install_requires=[],
+    extras_require={
+    },
+    py_modules=['seaborn.file'],
+    license='MIT License',
+    packages=['seaborn'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: Other/Proprietary License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6'],
+    entry_points='''
+        [console_scripts]
+        file=seaborn.file
+    ''',
+)
---
+++ test/test_file.py
@@ -0,0 +1,12 @@
+from seaborn.file import *
+import unittest
+
+class test_file(unittest.TestCase):
+    pass
+    #mkdir('%s/file_smoke_test/test1' % TRASH_PATH)
+    #assert os.path.exists('%s/file_smoke_test/test1' % TRASH_PATH)
+    #os.rmdir('%s/file_smoke_test/test1' % TRASH_PATH)
+    #os.rmdir('%s/file_smoke_test' % TRASH_PATH)
+
+if __name__ == "__main__":
+    unittest.main()
