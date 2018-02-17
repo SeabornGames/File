9de7b41570e288069ad99149fc35736e4e116267
MichaelChristenson <mochristenson@gmail.com>
2018-02-17 12:00:29 -0600

    S1 long desc (#5)
    * Create README.rst
    
    * Update setup.py
    
    * Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
 from setuptools import setup, find_packages
+import os
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as f:
+    long_description = f.read()
 
 setup(
     name='seaborn-file',
